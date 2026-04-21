c1787992c4e5b2b67d9306ee5393a4b10aef1498
Mamduh <mamduh2542@gmail.com>
2026-04-21 18:03:08 +0700

    add

--- src/game/systems/LevelUpDirector.ts
+++ src/game/systems/LevelUpDirector.ts
@@ -76,7 +76,7 @@ export class LevelUpDirector {
         (reward) =>
           reward.category === 'trait' &&
           reward.lane === 'bridge' &&
-          reward.heroBias !== heroId &&
+          reward.heroBias !== `shared` &&
           reward.traitId &&
           !selectedTraits.has(reward.traitId),
       ),
